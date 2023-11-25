fc0f7ef121f555bc3217aecfd64c50919076fe91
kafuipraise <matildapraise@gmail.com>
2023-11-25 04:35:39 -0800

    Task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Flask web application script """
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+def greet_hbnb():
+    """ Return a greeting message """
+    return 'Hello HBNB!'
+
+
+@app.route('/')
+def hello_hbnb():
+    """ Route for the root URL """
+    return greet_hbnb()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
