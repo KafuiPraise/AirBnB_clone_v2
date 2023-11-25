b0ca95c1cbbb662622f106b37b4a81475195b08c
kafuipraise <matildapraise@gmail.com>
2023-11-25 06:42:36 -0800

    All Task

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /cities_by_states: HTML page with a list of all State objects.
-"""
+"""Begins a Flask web application: Cities by states"""
+
 from flask import Flask, render_template
 from models import storage
+from models.city import City
 from models.state import State
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.teardown_appcontext
-def close_db(exc):
-    """Close the database session."""
-    storage.close()
-
-
 @app.route('/cities_by_states')
-def cities_by_states():
-    """Route to display an HTML page with a list of all State objects."""
+def cities_by_state():
+    """Renders an HTML page listing all states and their cities."""
     states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
 
-if __name__ == '__main__':
+@app.teardown_appcontext
+def teardown(exception):
+    """Closes the database session upon application context teardown."""
+    storage.close()
+
+
+if __name__ == "__main__":
     storage.reload()
-    app.run("0.0.0.0", 5000)
+    app.run(host='0.0.0.0', port=5000)
